686936d8a1b8a90ec0192941d8ca9d60fe87532d
MarcelDo01 <s0578647@htw-berlin.de>
2023-07-01 15:21:47 +0200

    first commit

---
+++ src/main/java/htwberlin/Karteikarten/KarteikartenApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.Karteikarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarteikartenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarteikartenApplication.class, args);
+	}
+
+}
