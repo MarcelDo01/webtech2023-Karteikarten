68d84eaf888468381719bd072991fec1d1e17575
MarcelDo01 <s0578647@htw-berlin.de>
2023-07-09 01:24:49 +0200

    asd

--- flashcard-frontend/src/components/Flashcard.vue
+++ flashcard-frontend/src/components/Flashcard.vue
@@ -5,13 +5,15 @@
       <p v-if="showAnswer" class="card-text">Antwort: {{ flashcard.answer }}</p>
       <div class="buttons">
         <button class="btn btn-danger" @click="deleteFlashcard">Delete</button>
-        <button class="btn btn-primary" @click="updateFlashcard">Update</button>
+        <button class="btn btn-primary" @click="openSidebar">Update</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'Flashcard',
   props: {
@@ -32,59 +34,7 @@ export default {
     deleteFlashcard() {
       // Implementiere hier die Logik zum Löschen der Karteikarte
       const flashcardId = this.flashcard.id;
-      // Rufen Sie den gewünschten API-Endpunkt auf, um die Karteikarte zu löschen
-      const endpoint = `http://localhost:8080/api/v1/flashcards/${this.flashcard.id}`;
-      const requestOptions = {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-
-      fetch(endpoint, requestOptions)
-        .then(response => {
-          if (response.ok) {
-            // Wenn die Karteikarte erfolgreich gelöscht wurde, können Sie hier weitere Aktionen ausführen
-            console.log('Karteikarte erfolgreich gelöscht');
-          } else {
-            // Wenn ein Fehler aufgetreten ist, können Sie hier den entsprechenden Fehler behandeln
-            console.error('Fehler beim Löschen der Karteikarte');
-          }
-        })
-        .catch(error => {
-          // Wenn ein Fehler beim Fetchen oder bei der Verarbeitung des Requests aufgetreten ist, können Sie ihn hier behandeln
-          console.error('Fehler beim Löschen der Karteikarte', error);
-        });
-    },
-    updateFlashcard() {
-      // Implementiere hier die Logik zur Aktualisierung der Karteikarte
-      const flashcardId = this.flashcard.id;
-      // Rufen Sie den gewünschten API-Endpunkt auf, um die Karteikarte zu aktualisieren
-      const endpoint = `http://localhost:8080/api/v1/flashcards/${this.flashcard.id}`;
-      const requestOptions = {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          // Passen Sie hier die aktualisierten Daten der Karteikarte an
-        })
-      };
-
-      fetch(endpoint, requestOptions)
-        .then(response => {
-          if (response.ok) {
-            // Wenn die Karteikarte erfolgreich aktualisiert wurde, können Sie hier weitere Aktionen ausführen
-            console.log('Karteikarte erfolgreich aktualisiert');
-          } else {
-            // Wenn ein Fehler aufgetreten ist, können Sie hier den entsprechenden Fehler behandeln
-            console.error('Fehler beim Aktualisieren der Karteikarte');
-          }
-        })
-        .catch(error => {
-          // Wenn ein Fehler beim Fetchen oder bei der Verarbeitung des Requests aufgetreten ist, können Sie ihn hier behandeln
-          console.error('Fehler beim Aktualisieren der Karteikarte', error);
-        });
+      // ...
     }
   }
 }
