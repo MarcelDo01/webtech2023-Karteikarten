9d3a828f0e633f1a3f582afb245ed88fc89133da
MarcelDo01 <s0578647@htw-berlin.de>
2023-07-09 01:38:54 +0200

    Delete button added

--- flashcard-frontend/src/components/EditFlashcardSidebar.vue
+++ flashcard-frontend/src/components/EditFlashcardSidebar.vue
@@ -1,13 +1,49 @@
 <template>
+  <button class="btn btn-success sticky-button" data-bs-toggle="offcanvas" data-bs-target="#flashcards-create-offcanvas" aria-controls="#flashcards-create-offcanvas">
+    <i class="bi bi-flashcard-plus-fill"></i>
+  </button>
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="flashcards-create-offcanvas" aria-labelledby="offcanvas-label">
+    <div class="offcanvas-header">
+      <h5 id="offcanvas-label">New Flashcard</h5>
+      <button type="button" id="close-offcanvas" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <form class="text-start needs-validation" id="flashcard-create-form" novalidate>
+        <div class="mb-3">
+          <label for="question" class="form-label">Question</label>
+          <input type="text" class="form-control" id="question" v-model="question" required>
+          <div class="invalid-feedback">
+            Please provide the question.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="answer" class="form-label">Answer</label>
+          <input type="text" class="form-control" id="answer" v-model="answer" required>
+          <div class="invalid-feedback">
+            Please provide the answer.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="category" class="form-label">Category</label>
+          <input id="category" class="form-select" v-model="category" required>
 
+          </input>
+          <div class="invalid-feedback">
+            Please select a valid gender.
+          </div>
+        </div>
+        <div v-if="this.serverValidationMessages">
+          <ul>
+            <li v-for="(message, index) in serverValidationMessages" :key="index" style="color: red">
+              {{ message }}
+            </li>
+          </ul>
+        </div>
+        <div class="mt-5">
+          <button class="btn btn-primary me-3" type="submit" @click.prevent="createPerson">Create</button>
+          <button class="btn btn-danger" type="reset">Reset</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-name: "EditFlashcardSidebar"
-}
-</script>
-
-<style scoped>
-
-</style>
--- flashcard-frontend/src/components/Flashcard.vue
+++ flashcard-frontend/src/components/Flashcard.vue
@@ -12,8 +12,6 @@
 </template>
 
 <script>
-
-
 export default {
   name: 'Flashcard',
   props: {
@@ -34,7 +32,34 @@ export default {
     deleteFlashcard() {
       // Implementiere hier die Logik zum Löschen der Karteikarte
       const flashcardId = this.flashcard.id;
-      // ...
+      // Rufen Sie den gewünschten API-Endpunkt auf, um die Karteikarte zu löschen
+      const endpoint = `http://localhost:8080/api/v1/flashcards/${flashcardId}`;
+      const requestOptions = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      fetch(endpoint, requestOptions)
+        .then(response => {
+          if (response.ok) {
+            // Wenn die Karteikarte erfolgreich gelöscht wurde, können Sie hier weitere Aktionen ausführen
+            console.log('Karteikarte erfolgreich gelöscht');
+            // Aktualisieren Sie die Liste der Karteikarten nach dem Löschen
+            this.$emit('cardDeleted', flashcardId);
+          } else {
+            // Wenn ein Fehler aufgetreten ist, können Sie hier den entsprechenden Fehler behandeln
+            console.error('Fehler beim Löschen der Karteikarte');
+          }
+        })
+        .catch(error => {
+          // Wenn ein Fehler beim Fetchen oder bei der Verarbeitung des Requests aufgetreten ist, können Sie ihn hier behandeln
+          console.error('Fehler beim Löschen der Karteikarte', error);
+        });
+    },
+    openSidebar() {
+      this.$emit('editCard', this.flashcard);
     }
   }
 }
